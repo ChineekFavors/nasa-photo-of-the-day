bfdc89b5a645fb1cd9c7a2c5e4667dc640bef428
Chineek Favors <chineekfavors01@gmail.com>
2019-08-01 12:49:19 -0700

    add components folder inside paragraph,image,heading folders

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-	const [state, setState] = useState('');
+	const [state, setState] = useState('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo');
 
 	useEffect( () => { 
 		axios.get()
 	});
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      // <p>
+      //   Read through the instructions in the README.md file to build your NASA
+      //   app! Have fun 🚀!
+      // </p>
     </div>
   );
 }
---
+++ src/components/paragraph/ParagraphCard.js
@@ -0,0 +1,1 @@
+ParagraphCard.js
